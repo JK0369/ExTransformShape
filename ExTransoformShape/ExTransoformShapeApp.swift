c45a3d055d4aa2e65b9714dc6c40fe80cddafb66
jake <palatable7@naver.com>
2022-10-13 21:24:40 +0900

    Initial Commit

---
+++ ExTransoformShape/ExTransoformShapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExTransoformShapeApp.swift
+//  ExTransoformShape
+//
+//  Created by 김종권 on 2022/10/13.
+//
+
+import SwiftUI
+
+@main
+struct ExTransoformShapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
