9e79774be6d9d416bd6842b7ff9d24a6083c8c7c
jake <palatable7@naver.com>
2022-10-13 21:25:11 +0900

    Implement

--- ExTransoformShape/ContentView.swift
+++ ExTransoformShape/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  var myTransformation: CGAffineTransform {
+    var affineTransform = CGAffineTransform(translationX: 100, y: 30)
+    affineTransform = affineTransform.scaledBy(x: 0.7, y: 0.7)
+    affineTransform = affineTransform.rotated(by: 45)
+    return affineTransform
+  }
+  
+  var body: some View {
+    VStack {
+      Group {
+//        ScaledShape(shape: Rectangle(), scale: CGSize(width: 0.7, height: 0.7))
+//        Divider()
+//        RotatedShape(shape: Rectangle(), angle: Angle(degrees: 45))
+//        Divider()
+//        OffsetShape(shape: Rectangle(), offset: CGSize(width: 100, height: 30))
+//        Divider()
+        TransformedShape(shape: Rectangle(), transform: myTransformation)
+      }
+      .foregroundColor(.blue)
+      .frame(width: 100, height: 100)
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
